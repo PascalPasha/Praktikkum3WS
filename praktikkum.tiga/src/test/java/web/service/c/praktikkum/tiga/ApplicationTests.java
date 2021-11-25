36d670d46cd53c18ed6e73651d1d5b9fab3b35a6
Pahlevi Pasha <pahlevi.pasha09@gmail.com>
2021-11-25 23:19:35 +0700

    Praktikkum3WS

---
+++ praktikkum.tiga/src/test/java/web/service/c/praktikkum/tiga/ApplicationTests.java
@@ -0,0 +1,13 @@
+package web.service.c.praktikkum.tiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
